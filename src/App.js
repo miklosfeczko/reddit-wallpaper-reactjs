85e8eace0dbb988fe72339cf2d9465b613a43002
Miklos Feczko <miklosfeczko01@gmail.com>
2019-08-24 23:35:42 +0200

    Made a basic API call with fetch.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import SearchBar from './components/SearchBar/SearchBar'
 import ImageList from './components/ImageList/ImageList'
 
 class App extends React.Component {
+  state = {
+    images: [],
+    BASE_URL: '//www.reddit.com/r/'
+  }
+
+  onSearchSubmit = async (term) => {
+    const response = await fetch(`${this.state.BASE_URL}${term}.json`);
+    const data = await response.json();
+    this.setState({ images: data.data.children });
+    console.log(this.state.images);
+  }
 
 render() {
   
   return (
     <div>
       Hello from App!
-      <SearchBar />
+      <SearchBar onSubmit={this.onSearchSubmit} />
       <ImageList />
     </div>
   )
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+    state = {term: ''}
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
     render() {
         return (
             <div>
-                Hello from SearchBar!
+               <form
+               onSubmit={this.onFormSubmit}
+               >
+                  <input
+                  value={this.state.term}
+                  onChange={e => this.setState({ term: e.target.value })}
+                  type="text" 
+                  /> 
+               </form>
             </div>
         )
     }
